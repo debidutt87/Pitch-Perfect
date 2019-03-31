00df29f303666325062e24e1c6929ecc8a98d8e8
Debi <debidutt.prasad@nl.abnamro.com>
2019-03-31 21:09:18 +0200

    controller implemented for playing the recorded audio

---
+++ Pitch Perfect/PlaySoundsViewController.swift
@@ -0,0 +1,16 @@
+//
+//  PlaySoundsViewController.swift
+//  Pitch Perfect
+//
+//  Created by Debidutt Prasad on 31/03/2019.
+//  Copyright © 2019 Bot Consultancy. All rights reserved.
+//
+
+import UIKit
+
+class PlaySoundsViewController:UIViewController{
+    
+    override func viewDidLoad() {
+        
+    }
+}
